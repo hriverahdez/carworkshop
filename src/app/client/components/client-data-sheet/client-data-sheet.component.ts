0d3c8079fbc9d551e68f2e762bfc478d1c4dfb7a
Helian Rivera <hriverahdez2011@gmail.com>
2018-07-05 17:15:58 -0400

    HOTFIX: Fixed issue on fields visibility toggling not working on client profile due to production build minification.

--- src/app/client/components/client-data-sheet/client-data-sheet.component.ts
+++ src/app/client/components/client-data-sheet/client-data-sheet.component.ts
@@ -14,8 +14,107 @@ export class ClientDataSheetComponent implements OnInit {
 
   ngOnInit() {}
 
-  fieldIsVisible(fieldName: string) {
+  // DOESN'T WORK WHEN MINIFIED
+  //   fieldIsVisible(fieldName: string) {
+  //     if (this.userIsAdmin) return true;
+  //     return this.car && this.car[fieldName] && this.car[`${fieldName}IsVisible`];
+  //   }
+
+  get modelVisible() {
     if (this.userIsAdmin) return true;
-    return this.car && this.car[fieldName] && this.car[`${fieldName}IsVisible`];
+    return this.car && this.car.model && this.car.modelIsVisible;
+  }
+
+  get vinNumberVisible() {
+    if (this.userIsAdmin) return true;
+    return this.car && this.car.vinNumber && this.car.vinNumberIsVisible;
+  }
+
+  get variantVisible() {
+    if (this.userIsAdmin) return true;
+    return this.car && this.car.variant && this.car.variantIsVisible;
+  }
+
+  get typeVisible() {
+    if (this.userIsAdmin) return true;
+    return this.car && this.car.type && this.car.typeIsVisible;
+  }
+
+  get versionVisible() {
+    if (this.userIsAdmin) return true;
+    return this.car && this.car.version && this.car.versionIsVisible;
+  }
+
+  get engineBrandVisible() {
+    if (this.userIsAdmin) return true;
+    return this.car && this.car.engineBrand && this.car.engineBrandIsVisible;
+  }
+
+  get engineCodeVisible() {
+    if (this.userIsAdmin) return true;
+    return this.car && this.car.engineCode && this.car.engineCodeIsVisible;
+  }
+
+  get tiresVisible() {
+    if (this.userIsAdmin) return true;
+    return this.car && this.car.tires && this.car.tiresIsVisible;
+  }
+
+  get cilinderAmountVisible() {
+    if (this.userIsAdmin) return true;
+    return (
+      this.car && this.car.cilinderAmount && this.car.cilinderAmountIsVisible
+    );
+  }
+
+  get cilinderVolumeVisible() {
+    if (this.userIsAdmin) return true;
+    return (
+      this.car && this.car.cilinderVolume && this.car.cilinderVolumeIsVisible
+    );
+  }
+
+  get potencyKWVisible() {
+    if (this.userIsAdmin) return true;
+    return this.car && this.car.potencyKW && this.car.potencyKWIsVisible;
+  }
+
+  get potencyCVVisible() {
+    if (this.userIsAdmin) return true;
+    return this.car && this.car.potencyCV && this.car.potencyCVIsVisible;
+  }
+
+  get emisionLevelVisible() {
+    if (this.userIsAdmin) return true;
+    return this.car && this.car.emisionLevel && this.car.emisionLevelIsVisible;
+  }
+
+  get fuelVisible() {
+    if (this.userIsAdmin) return true;
+    return this.car && this.car.fuel && this.car.fuelIsVisible;
+  }
+
+  get radioCodeVisible() {
+    if (this.userIsAdmin) return true;
+    return this.car && this.car.radioCode && this.car.radioCodeIsVisible;
+  }
+
+  get keyCodeVisible() {
+    if (this.userIsAdmin) return true;
+    return this.car && this.car.keyCode && this.car.keyCodeIsVisible;
+  }
+
+  get observationsVisible() {
+    if (this.userIsAdmin) return true;
+    return this.car && this.car.observations && this.car.observationsIsVisible;
+  }
+
+  get maintenanceResetVisible() {
+    if (this.userIsAdmin) return true;
+    return (
+      this.car &&
+      this.car.maintenanceReset &&
+      this.car.maintenanceResetIsVisible
+    );
   }
 }
